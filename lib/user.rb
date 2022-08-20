38f5fde1e9376f3a3561b54466773351c5acdd16
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-21 01:24:39 +0300

    commit tree c02960fe9725cd9c0183d12fa5169699c7add91b

---
+++ lib/user.rb
@@ -0,0 +1,25 @@
+class User
+
+    # attr_accessor :first_name, :last_name
+
+    # def initialize(first_name, last_name)
+    #     @first_name = first_name
+    #     @last_name = last_name
+    # end
+
+    def first_name=(value)
+        @first_name = value
+    end
+
+    def last_name=(value)
+        @last_name = value
+    end
+
+    def first_name
+        @first_name
+    end
+
+    def last_name
+        @last_name
+    end
+end
